a8a6a0fe6a0fd35f25a4f1a3f18d1868800abc88
Fae-new <faluyiaji@gmail.com>
2022-06-17 23:53:16 +0100

    completed version and functionality finishing touches left

---
+++ src/components/navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+const Navbar=()=>{
+    return(
+        <nav className="navbar">
+        <div style={{display:'flex',flexDirection:'row',alignItems:'center'}}>
+      
+        <div style={{margin:'0px 15px'}}>
+<div className="box animate" style={{backgroundColor:'#fffaff'}}></div>
+<div  className="box animate" style={{backgroundColor:"#303036",transform: 'rotate(-45deg)',position:'absolute',marginTop:'-30px'}}></div>
+
+</div>
+
+<h2>Where in the world ? </h2>
+</div>       
+ </nav>
+    )
+}
+
+export default Navbar;
---
+++ src/components/searchbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+const Searchbar=({onChangeRegion,onChangeSearch,searchValue,filterValue})=>{
+return(
+  <div>
+ 
+<div className="Searchbar">
+
+<input type='text' placeholder="Search for a country" value={searchValue}onChange={onChangeSearch}/>
+<h4 className="filtertext">Filter by region</h4>
+<select id="Region" name="Region" onChange={onChangeRegion} value={filterValue}>
+  <option value={null} defaultValue></option>
+  <option value="Africa">Africa</option>
+  <option value="America">America</option>
+  <option value="Asia">Asia</option>
+  <option value="Europe">Europe</option>
+  <option value="Oceania">Oceania</option>
+ 
+</select>
+
+</div>
+</div>
+)
+}
+
+export default Searchbar;
