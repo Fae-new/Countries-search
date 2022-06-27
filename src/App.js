1a2eede8355d3b648af7fdf6bf4b3c90720cbece
Fae-new <faluyiaji@gmail.com>
2022-06-27 08:38:41 +0100

    minor changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import Navbar from './components/navbar';
 import Body from './components/body';
-import Footer from './components/footer';
+// import Footer from './components/footer';
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
 import CountryDetails from './components/countrydetails';
 
@@ -23,7 +23,7 @@ function App() {
   
 </Switch>
 
-<Footer/>
+{/* <Footer/> */}
     </div>
     </Router>
   );
